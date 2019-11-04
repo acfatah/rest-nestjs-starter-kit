0c662aa3a306d1620a8ca9ea3023fb9064ab4107
Achmad F. Ibrahim <acfatah@gmail.com>
2019-11-04 13:01:51 +0800

    Refactor ConfigService
    .env file is no longer mandatory.

--- src/config/config.service.spec.ts
+++ src/config/config.service.spec.ts
@@ -1,15 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { ConfigService } from './config.service'
-import { existsSync } from 'fs'
 describe('ConfigService', () => {
   let provider: ConfigService
 
   beforeEach(async () => {
-    if (!(await existsSync('.env'))) {
-      // stop the test if '.env file is not defined'
-      process.exit(1)
-    }
-
     const module: TestingModule = await Test.createTestingModule({
       providers: [ConfigService],
     }).compile()
--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -1,12 +1,14 @@
 import { config, parse } from 'dotenv'
-import { readFileSync } from 'fs'
+import { existsSync, readFileSync } from 'fs'
 
 export class ConfigService {
   private readonly envConfig: { [key: string]: string }
 
   constructor(filePath: string = '.env') {
-    config({ path: filePath })
-    this.envConfig = parse(readFileSync(filePath))
+    if (existsSync(filePath)) {
+      config({ path: filePath })
+      this.envConfig = parse(readFileSync(filePath))
+    }
   }
 
   get(key: string): string {
