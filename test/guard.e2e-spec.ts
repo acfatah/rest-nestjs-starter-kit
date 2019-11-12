5b628b4e6ad51ebd9f6b655677648b64957ce6eb
Achmad F. Ibrahim <acfatah@gmail.com>
2019-11-12 09:47:58 +0800

    Add guard e2e test as an example

---
+++ test/guard.e2e-spec.ts
@@ -0,0 +1,45 @@
+import {
+  INestApplication,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common'
+import { APP_GUARD } from '@nestjs/core'
+import { Test } from '@nestjs/testing'
+import * as request from 'supertest'
+import { AppModule } from '../src/app.module'
+import { ConfigModule } from '../src/config/config.module'
+
+@Injectable()
+export class AuthGuard {
+  canActivate() {
+    const x = true
+    if (x) {
+      throw new UnauthorizedException()
+    }
+  }
+}
+
+function createTestModule(guard) {
+  return Test.createTestingModule({
+    imports: [AppModule, ConfigModule],
+    providers: [
+      {
+        provide: APP_GUARD,
+        useValue: guard,
+      },
+    ],
+  }).compile()
+}
+
+describe('Guards', () => {
+  let app: INestApplication
+
+  it(`should prevent access (unauthorized)`, async () => {
+    app = (await createTestModule(new AuthGuard())).createNestApplication()
+
+    await app.init()
+    return request(app.getHttpServer())
+      .get('/')
+      .expect(401)
+  })
+})
